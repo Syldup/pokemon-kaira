5b5cf8b87e7c6e156971f27eac08d548c2df79b7
Ludo <kiluhasenpai@gmail.com>
2020-02-02 20:39:01 +0100

    create pokemon

--- pokemon.py
+++ pokemon.py
@@ -1,5 +1,26 @@
 import json
 
 
+
+
 class Pokemon():
-def __init__(self, name)
+    def __init__(self, name, hp, attack, defense, specialAttack, specialDefense, speed):
+        with open('resources.json') as json_file:
+            data = json.load(json_file)
+            for p in data['results']:
+                self.name = p['name']
+        self.hp = []
+        self.attack = []
+        self.defense = []
+        self.specialAttack = []
+        self.specialDefense = []
+        self.speed = []
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+
+
+
+
+
