3cc305e8213e5037d37e82b455c448af35db97b8
Syldup <syd9up@gmail.com>
2020-02-06 10:21:29 +0100

    fix button pos

--- pageCombat.py
+++ pageCombat.py
@@ -3,44 +3,39 @@ from utils import *
 
 class PageCombat:
     def __init__(self, screen):
-        w, h = screen.get_size()
-        self.y_tier = int(h/3)
-        self.bg_combat = pygame.Surface((w, self.y_tier*2))
-        self.bg_menu = pygame.Surface((w, h-self.y_tier*2))
+        self.bg_rect = screen.get_rect()
+        self.combat_rect = screen.get_rect()
+        self.combat_rect[3] = int(2*self.bg_rect[3]/3)
+        self.bg = pygame.Surface(self.bg_rect.size)
         self.loop = True
         self.level = 1
 
         self.fond_combat = scale(loadImg('bg/combat1.png'), 2.51)
-        self.bot_combat = pygame.transform.scale(loadImg('bg/bot_combat.jpg'), self.bg_menu.get_size())
+        self.bot_combat = pygame.transform.scale(loadImg('bg/bot_combat.jpg'), self.combat_rect.size)
         self.box_left = loadImg('sprite/brick-wallpaper250px.png')
         self.box_right = pygame.transform.rotate(loadImg('sprite/brick-wallpaper250px.png'), 180)
 
         self.btn_conf = {
-            'bg': self.bg_menu,
+            'bg': self.bg,
             'overflew': True,
             'font': pygame.font.Font('assets/Pixeled.ttf', 18),
             'text_offset': None,
             'sprite': {
                 True: loadImg('icon/select_on.png'),
-                False: loadImg('icon/select_off.png'),
             },
+            'box_size': True,
         }
-        w, h = self.bg_menu.get_size()
         self.btns = [
-            (Button(self.btn_conf, 'ATTAQUE', (w-20, h/2-10), origine=9), self.attaque),
-            (Button(self.btn_conf, 'POKEMON', (w-20, h/2+10), origine=3), self.equpe),
+            (Button(self.btn_conf, 'ATTAQUE', (self.bg_rect[2]-10, 5*self.bg_rect[3]/6-8), origine=9), self.attaque),
+            (Button(self.btn_conf, 'POKEMON', (self.bg_rect[2]-10, 5*self.bg_rect[3]/6+8), origine=3), self.equpe),
         ]
 
     def draw(self):
-        centerx = self.bg_combat.get_rect().centerx
-        centery = self.bg_combat.get_rect().centery
-        w, h = self.bg_combat.get_rect().size
+        drawOn(self.bg, self.fond_combat, (self.combat_rect.centerx, self.combat_rect[3]), origine=8)
+        drawOn(self.bg, self.box_left, (0, 50), origine=1)
+        drawOn(self.bg, self.box_right, (self.combat_rect[2], 220), origine=3)
 
-        drawOn(self.bg_combat, self.fond_combat, (centerx, h), origine=8)
-        drawOn(self.bg_combat, self.box_left, (0, 50), origine=1)
-        drawOn(self.bg_combat, self.box_right, (w, 220), origine=3)
-
-        drawOn(self.bg_menu, self.bot_combat, (centerx, 0), origine=2)
+        drawOn(self.bg, self.bot_combat, (self.combat_rect.centerx, self.combat_rect[3]), origine=2)
         for btn, action in self.btns:
             btn.draw()
 
@@ -53,8 +48,7 @@ class PageCombat:
         pass
 
     def display(self, screen):
-        screen.blit(self.bg_combat, (0, 0))
-        screen.blit(self.bg_menu, (0, self.y_tier*2))
+        screen.blit(self.bg, (0, 0))
 
     def attaque(self):
         print("attaque")
